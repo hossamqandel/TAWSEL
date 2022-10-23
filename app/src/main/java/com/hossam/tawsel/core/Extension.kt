a3a7bc3c9cc5fd9a1f3d5e20ef33bc0c9c437ac3
Hossam <hossamegyqandel@gmail.com>
2022-10-23 13:29:14 +0200

    Add statusBar and icons color

--- app/src/main/java/com/hossam/tawsel/core/Extension.kt
+++ app/src/main/java/com/hossam/tawsel/core/Extension.kt
@@ -1,22 +1,31 @@
 package com.hossam.tawsel.core
 
 import android.content.Context
+import android.content.res.Resources
+import android.graphics.Color
 import android.os.Handler
 import android.os.Looper
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.view.View
+import android.view.Window
+import android.view.WindowManager
 import android.widget.EditText
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.snackbar.Snackbar
+import com.hossam.tawsel.R
+
+private val TAG = "Extension"
 
 fun handler(timer: Long, block: () -> Unit){
     Handler(Looper.getMainLooper()).postDelayed({
         block()
     }, timer)
 }
+
 
 infix fun EditText.onTextChanged(onTextChanged: (String) -> Unit) {
     this.addTextChangedListener(object : TextWatcher {
@@ -48,6 +57,30 @@ fun Fragment.navigate(navId: Int){
     findNavController().navigate(navId)
 }
 
+
 fun currentThread(): String {
     return Thread.currentThread().toString()
+}
+
+fun Fragment.changeStatusBarIconsColor(colorId: Int){
+    requireActivity().window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+    requireActivity().window.statusBarColor = colorId
+}
+
+fun Fragment.changeStatusBarColor(colorId: Int){
+    try {
+        val window: Window = requireActivity().window
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        window.statusBarColor = resources.getColor(colorId)
+    }catch (e: Resources.NotFoundException){
+        Log.i(TAG, "changeStatusBarColor: $e")
+    } catch (e: NullPointerException){
+        Log.i(TAG, "changeStatusBarColor: $e")
+    }
+}
+
+fun Fragment.setupStatusBarWithIcons(statusColor: Int, iconColor: Int){
+    changeStatusBarColor(statusColor)
+    changeStatusBarIconsColor(iconColor)
 }

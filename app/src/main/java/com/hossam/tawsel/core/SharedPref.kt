6ad2e136502af6b482e7689f0a90c37d6973e969
Hossam <hossamegyqandel@gmail.com>
2022-10-23 17:08:49 +0200

    implement shared prefs class

--- app/src/main/java/com/hossam/tawsel/core/SharedPref.kt
+++ app/src/main/java/com/hossam/tawsel/core/SharedPref.kt
@@ -7,7 +7,14 @@ object SharedPref {
 
     private var mAppContext: Context? = null
     private const val SHARED_PREFERENCES_NAME = "tawsel data"
-    private const val USER_PHONE = "mobile"
+    private const val USER_NAME = "name"
+    private const val USER_PHONE = "phone"
+    private const val USER_EMAIL = "email"
+    private const val USER_ADDRESS = "address"
+    private const val USER_LOGO = "logo"
+    private const val USER_UPDATED_AT = "updated_at"
+    private const val USER_CREATED_AT = "created_at"
+    private const val USER_ID = "id"
 
 
     fun init(appContext: Context?) {
@@ -18,10 +25,11 @@ object SharedPref {
         return mAppContext!!.getSharedPreferences(SHARED_PREFERENCES_NAME, Context.MODE_PRIVATE)
     }
 
-    fun setUserPhone (value : String){
+    fun setUserName (value : String){
         val editor = getSharedPreferences().edit()
-        editor.putString(USER_PHONE, value).apply()
+        editor.putString(USER_NAME, value).apply()
     }
+    fun getUserName ():String = getSharedPreferences().getString(USER_NAME , "")!!
 
-    fun getUserPhone ():String = getSharedPreferences().getString(USER_PHONE , "")!!
+
 }

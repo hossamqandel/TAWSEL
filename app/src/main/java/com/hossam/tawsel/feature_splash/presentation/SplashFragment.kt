c9e482229f001332f0dac41affe2d54650f1a8e0
Hossam <hossamegyqandel@gmail.com>
2022-10-23 17:07:24 +0200

    implement splash screen

--- app/src/main/java/com/hossam/tawsel/feature_splash/presentation/SplashFragment.kt
+++ app/src/main/java/com/hossam/tawsel/feature_splash/presentation/SplashFragment.kt
@@ -1,19 +1,29 @@
 package com.hossam.tawsel.feature_splash.presentation
 
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
+import androidx.fragment.app.Fragment
 import com.hossam.tawsel.R
+import com.hossam.tawsel.core.changeStatusBarIconsColor
 import com.hossam.tawsel.databinding.FragmentSplashBinding
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class SplashFragment : Fragment() {
 
     private var _binding: FragmentSplashBinding? = null
     private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //        setupStatusBarWithIcons(Anti_Flash_White, BLACK) //TODO Ext fun not worked but selfCode works fine
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,10 +35,16 @@ class SplashFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
+
+
+
 }

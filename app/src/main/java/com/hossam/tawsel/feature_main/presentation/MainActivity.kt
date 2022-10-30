9374f2b83cac8bc673ad1e34b37eaccb94f49fd3
Hossam <hossamegyqandel@gmail.com>
2022-10-30 23:59:03 +0200

    setup and implement bottom Nav View with needed assets, tools

--- app/src/main/java/com/hossam/tawsel/feature_main/presentation/MainActivity.kt
+++ app/src/main/java/com/hossam/tawsel/feature_main/presentation/MainActivity.kt
@@ -2,15 +2,43 @@ package com.hossam.tawsel.feature_main.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.messaging.FirebaseMessaging
 import com.hossam.tawsel.R
+import com.hossam.tawsel.core.SharedPref
+import com.hossam.tawsel.feature_main.data.remote.ITawselService
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+        setupBottomNavigationView()
+
+
+    }
+
+
+    private fun setupBottomNavigationView(){
+        val bottomNavView = findViewById<BottomNavigationView>(R.id.bottomNavView)
+        val navController = Navigation.findNavController(this, R.id.fragmentContainerView)
+        bottomNavView.setupWithNavController(navController)
+        NavigationUI.setupWithNavController(bottomNavView, navController)
     }
 }
